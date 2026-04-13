fda89f442d420d7b8e73baa4b635547527b16dbe
developer01-de <fynnbrederlow@online.de>
2026-04-13 09:30:54 +0200

    Jump implemented, NOT working

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Player : MonoBehaviour
+{
+    private InputAction _jumpAction;
+
+    private Rigidbody2D _body;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        _jumpAction = InputSystem.actions.FindAction("Jump");
+        _body = new Rigidbody2D();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (_jumpAction.WasPerformedThisFrame())
+        {
+            _body.AddForce(Vector2.up);
+        }
+    }
+}
