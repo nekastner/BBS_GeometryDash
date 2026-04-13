720f0aeb7eff6c16a35e184eb5a15a2c0a330354
developer01-de <fynnbrederlow@online.de>
2026-04-13 21:04:39 +0200

    InputAction and PlayerInput implemented, but do not work

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,22 +2,33 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
-    private InputAction _jumpAction;
-
-    private Rigidbody2D _body;
+    private const float JumpForce = 1;
+    public InputAction jumpAction;
+    public Rigidbody2D body;
+    public PlayerInput playerInput;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        _jumpAction = InputSystem.actions.FindAction("Jump");
-        _body = new Rigidbody2D();
+        body = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+        jumpAction = playerInput.actions["Jump"];
+        jumpAction.performed += OnJugitmp;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (_jumpAction.WasPerformedThisFrame())
-        {
-            _body.AddForceY(1);
-        }
+        
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed) Jump();
+    }
+
+    private void Jump()
+    {
+        body.AddForceY(JumpForce);
     }
 }
