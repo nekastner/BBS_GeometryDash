68c1019c6baf81448b9cbb49209927203473a51f
Noah Elias Kastner <elias@kastner-home.com>
2026-04-20 09:37:30 +0200

    player can jump now

--- Assets/Resources/PlayerScript.cs
+++ Assets/Resources/PlayerScript.cs
@@ -1,7 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
 namespace Resources
 {
-    public class Player
+    public class PlayerScript : MonoBehaviour
     {
-        
+        [SerializeField] private InputManagementScript ims;
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private LogicScript logic;
+        public float jumpForce = 1;
+    
+        public void Start()
+        {
+            ims.Controls.Player.Jump.performed += Jump;
+        }
+
+        public void Update()
+        {
+            if (logic.GameIsOver())
+            {
+                ims.Controls.Player.Jump.performed -= Jump;
+            }
+        }
+
+        private void Jump(InputAction.CallbackContext ctx)
+        {
+            rb.linearVelocity = Vector2.up * jumpForce;
+        }
+
+        private void OnCollisionEnter2D (Collision2D collision)
+        {
+            //logic.GameOver();
+        }
     }
 }
