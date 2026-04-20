9cf6026237bccfd8735b329bc7015a35a56ee9a0
Noah Elias Kastner <elias@kastner-home.com>
2026-04-20 09:29:11 +0200

    added game logic script

---
+++ Assets/GameLogicScript.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    [SerializeField] private InputManagementScript ims;
+    
+    private bool _gameStarted;
+    private bool _gamePaused;
+    private bool _gameOver;
+
+    private int _score;
+    private int _highScore;
+
+    public void Start()
+    {
+        // TODO: subscribe to input system
+        ims.Controls.Player.Jump.performed += StartGame;
+        Time.timeScale = 0;
+    }
+
+    public bool GameIsOver()
+    {
+        return _gameOver;
+    }
+
+    private void StartGame(InputAction.CallbackContext ctx)
+    {
+        if (_gameStarted) return;
+        _gameStarted = true;
+        // TODO: unsubscribe from input system
+        Time.timeScale = 1;
+    }
+    
+    private void UnstartGame()
+    {
+        _gameStarted = false;
+        Time.timeScale = 0;
+        // TODO: set home screen
+    }
+
+    private void TogglePause(InputAction.CallbackContext context)
+    {
+        if (_gamePaused) UnpauseGame();
+        else PauseGame();
+    }
+
+    private void PauseGame()
+    {
+        _gamePaused = true;
+        Time.timeScale = 0;
+        // TODO: set pause screen
+    }
+
+    public void UnpauseGame()
+    {
+        _gamePaused = false;
+        Time.timeScale = 1;
+        // TODO: set game screen
+
+        if (_gameOver) GameOver();
+        if (!_gameStarted) UnstartGame();
+    }
+
+    public void GameOver()
+    {
+        _gameOver = true;
+        // TODO: set game over screen
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
