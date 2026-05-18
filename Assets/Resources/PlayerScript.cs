ced48374d46afa34ecbc76de9d9d445bad9513d5
developer01-de <fynnbrederlow@online.de>
2026-05-18 08:28:05 +0200

    Double Jump is not possible anymore

--- Assets/Resources/PlayerScript.cs
+++ Assets/Resources/PlayerScript.cs
@@ -8,6 +8,7 @@ namespace Resources
         [SerializeField] private InputManagementScript ims;
         [SerializeField] private Rigidbody2D rb;
         [SerializeField] private LogicScript logic;
+        private bool hasContact;
         public float jumpForce = 1;
     
         public void Start()
@@ -25,12 +26,14 @@ namespace Resources
 
         private void Jump(InputAction.CallbackContext ctx)
         {
+            if (!hasContact) return;
             rb.linearVelocity = Vector2.up * jumpForce;
+            hasContact = false;
         }
 
         private void OnCollisionEnter2D (Collision2D collision)
         {
-            //logic.GameOver();
+            hasContact = true;
         }
     }
 }
