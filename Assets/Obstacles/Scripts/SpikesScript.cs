13ef6e652ecb59fed38927241b2ab88b27949f34
Noah Elias Kastner <elias@kastner-home.com>
2026-05-18 09:44:04 +0200

    spikes now spawn in noahs space

--- Assets/Obstacles/Scripts/SpikesScript.cs
+++ Assets/Obstacles/Scripts/SpikesScript.cs
@@ -1,36 +1,59 @@
-using TMPro;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Obstacles.Scripts
 {
     public class SpikesScript : MonoBehaviour
     {
-        public uint spikesAmount = 1;
+        [SerializeField] public float speed;
+        [SerializeField] public float spawnTimeDistance;
+        
         private Object _spikePrefab;
+        private float _timeSinceLastSpawn;
+        private readonly List<Object> _spawnedObjects = new();
     
         public void Start()
         {
-            // load prefabs
-            _spikePrefab = UnityEngine.Resources.Load("Spike");
-            
-            // spawn spikes
-            var spawnPosition = transform.position;
-            for (var i = 0; i < spikesAmount; i++)
-            {
-                SpawnSpike(spawnPosition);
-                spawnPosition.x++;
-            }
+            this._spikePrefab = UnityEngine.Resources.Load("Spike");
         }
     
         public void Update()
         {
-            // TODO: move spikes
+            var deltaTime = Time.deltaTime;
+            this._timeSinceLastSpawn += deltaTime;
+            
+            foreach (var spawnedObject in this._spawnedObjects)
+                spawnedObject.GameObject().transform.Translate(Vector3.left * (this.speed * deltaTime));
+
+            if (this._timeSinceLastSpawn < this.spawnTimeDistance) return;
+            
+            this.SpawnRandomAmountOfSpikes();
+            this._timeSinceLastSpawn = 0;
         }
 
-        private void SpawnSpike(Vector3 position)
+        private void SpawnSingleSpike(Vector3 position)
         {
-            if (_spikePrefab is null) return;
-            Instantiate(_spikePrefab, position, Quaternion.identity);
+            if (this._spikePrefab is null)
+                throw new System.Exception("Spike prefab is null!");
+            
+            this._spawnedObjects.Add(Instantiate(this._spikePrefab, position, Quaternion.identity));
+        }
+
+        private void SpawnSpecificAmountOfSpikes(byte amount)
+        {
+            var spawnPosition = this.transform.position;
+            for (var i = 0; i < amount; i++)
+            {
+                this.SpawnSingleSpike(spawnPosition);
+                spawnPosition.x++;
+            }
+        }
+
+        private void SpawnRandomAmountOfSpikes()
+        {
+            var amountOfSpikes = (byte) Random.Range(1, 3);
+            this.SpawnSpecificAmountOfSpikes(amountOfSpikes);
         }
     }
 }
